c62230845e266f5f4458d01a07a22dbac4985441
sgolodetz <sgolodetz@gxstudios.net>
2011-11-12 13:56:10 +0000

    Wrote a simple test program.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics.Contracts;
 
 namespace ContractTest
 {
+	class C
+	{
+		public static void OutputPositiveInt(int i)
+		{
+			Contract.Requires(i >= 0);
+			Console.WriteLine(i);
+		}
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			C.OutputPositiveInt(-23);
 		}
 	}
 }
